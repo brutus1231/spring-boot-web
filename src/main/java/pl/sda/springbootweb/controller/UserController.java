68c106de3df67b9dd20b1eeb23d9a246dc88e4c0
zdzislaw trzaska <ztrzaska3@gmail.com>
2020-11-14 13:14:44 +0100

    user service

--- src/main/java/pl/sda/springbootweb/controller/UserController.java
+++ src/main/java/pl/sda/springbootweb/controller/UserController.java
@@ -1,9 +1,23 @@
 package pl.sda.springbootweb.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.sda.springbootweb.service.UserService;
 
 @Controller
 @RequestMapping("/users")
+@RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping
+    public String users(Model model) {
+        model.addAttribute("users", userService.list());
+        return "users";
+    }
+
 }
