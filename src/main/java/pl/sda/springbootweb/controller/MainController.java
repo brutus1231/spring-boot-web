17ceff775ebbadd5c0d3fb00bcb38315a7e28384
zdzislaw trzaska <ztrzaska3@gmail.com>
2020-11-14 11:21:56 +0100

    ModelAndView i string

--- src/main/java/pl/sda/springbootweb/controller/MainController.java
+++ src/main/java/pl/sda/springbootweb/controller/MainController.java
@@ -1,6 +1,8 @@
 package pl.sda.springbootweb.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -8,12 +10,13 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class MainController {
 
-//    @RequestMapping("/", method = RequestMethod.GET)
-//    public String mainPage() {
-//        return "index";
-//    }
+    @GetMapping("/strona2")
+    public String mainPage2(Model model) {
+        model.addAttribute("variable", "ide do parku");
+        return "index";
+    }
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @GetMapping("/")
     public ModelAndView mainPage() {
         ModelAndView mv = new ModelAndView();
         mv.setViewName("index");
