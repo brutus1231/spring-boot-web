2db4d79ca00a56b70008b76230b59858690ab500
zdzislaw trzaska <ztrzaska3@gmail.com>
2020-11-14 11:52:16 +0100

    spring mvc

--- src/main/java/pl/sda/springbootweb/controller/MainController.java
+++ src/main/java/pl/sda/springbootweb/controller/MainController.java
@@ -3,6 +3,7 @@ package pl.sda.springbootweb.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -24,7 +25,7 @@ public class MainController {
         return mv;
     }
 
-    @GetMapping("/strona3")
+    @PostMapping("/strona3")
     public String buttonClicked(Model model) {
         model.addAttribute("joinedText3", "xxx");
         return "index";
